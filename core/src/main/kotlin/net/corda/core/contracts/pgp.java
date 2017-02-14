c3bcd442a48df946f9a72786829c3329b9b2eb41
James Sangalli <j.l.sangalli@gmail.com>
2017-02-15 10:04:25 +1100

    didisoft boilerplate added

---
+++ core/src/main/kotlin/net/corda/core/contracts/pgp.java
@@ -0,0 +1,44 @@
+package net.corda.core.contracts;
+
+
+
+/**
+ * Created by sangalli on 15/2/17.
+ */
+public class pgp
+{
+
+
+
+//    public static void generateKey() throws Exception
+//    {
+//        // initialize the KeyStore instance
+//        KeyStore ks = new KeyStore("mypgp.keystore", "my store pass");
+//
+//        // EC curve for this key
+//        String curve = EcCurve.P384;
+//        // User Id for this key
+//        String userId = "my name";
+//
+//        KeyPairInformation newKey = ks.generateEccKeyPair(curve, userId, "my key password");
+//
+//        // now the public key can be exported and sent to our partners
+//    }
+//
+//    public static void encrypt (String[] args) throws Exception{
+//        // create an instance of the library
+//        PGPLib pgp = new PGPLib();
+//
+//        // is output ASCII or binary
+//        boolean asciiArmor = false;
+//        // should integrity check information be added
+//        // set to false for compatibility with older versions of PGP such as 6.5.8.
+//        boolean withIntegrityCheck = false;
+//
+//        pgp.encryptFile("INPUT.txt",
+//                "public.key",
+//                "OUTPUT.pgp",
+//                asciiArmor,
+//                withIntegrityCheck);
+//    }
+}
